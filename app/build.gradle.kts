5e6f7b8e21bd5db2f12d15e1615e57231e833e9b
Todokanai <bh951201@gmail.com>
2024-07-29 20:08:47 +0900

    Gradle dependency cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,17 +62,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.google.android.material:material:1.6.1")
@@ -83,10 +77,7 @@ dependencies {
     implementation ("androidx.room:room-ktx:$roomVersion")
 
     implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation ("androidx.activity:activity-compose:1.5.1")
     implementation ("androidx.compose.ui:ui:1.5.0-alpha04")
-    implementation ("androidx.compose.ui:ui-graphics")
-    implementation ("androidx.compose.ui:ui-tooling-preview")
     implementation ("androidx.compose.material3:material3:1.1.0")
     implementation ("androidx.compose.runtime:runtime-livedata:1.4.3")
 
