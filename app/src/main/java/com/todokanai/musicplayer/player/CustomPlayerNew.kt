fed458b9703d4b81c129e557778d3ed1acfe1225
Todokanai <bh951201@gmail.com>
2024-10-27 18:09:20 +0900

    Room에서 직접 flow 끌어오는 방식으로 Rollback 할 것

--- app/src/main/java/com/todokanai/musicplayer/player/CustomPlayerNew.kt
+++ app/src/main/java/com/todokanai/musicplayer/player/CustomPlayerNew.kt
@@ -1,18 +1,45 @@
 package com.todokanai.musicplayer.player
 
+import android.content.Context
 import android.media.MediaPlayer
 import com.todokanai.musicplayer.data.datastore.DataStoreRepository
 import com.todokanai.musicplayer.data.room.Music
+import com.todokanai.musicplayer.myobjects.MyObjects.nextIntent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
 abstract class CustomPlayerNew(val dataStoreRepository:DataStoreRepository) {
 
+    private val scope = CoroutineScope(Dispatchers.Default)
     val mediaPlayer = MediaPlayer()
 
-    abstract val isShuffledHolder: StateFlow<Boolean>
+    private val initialLoop = false
+    private val initialShuffle = false
+    private val initialSeed : Double = 0.1
 
-    abstract val isLoopingHolder: StateFlow<Boolean>
+    val isShuffledHolder: StateFlow<Boolean> =
+        dataStoreRepository.isShuffled.stateIn(
+            scope = scope,
+            started = SharingStarted.WhileSubscribed(5),
+            initialValue = initialShuffle
+        )
+    val isLoopingHolder: StateFlow<Boolean> =
+        dataStoreRepository.isShuffled.stateIn(
+            scope = scope,
+            started = SharingStarted.WhileSubscribed(5),
+            initialValue = initialLoop
+        )
+
+    val seedHolder : StateFlow<Double> =
+        dataStoreRepository.seed.stateIn(
+            scope = scope,
+            started = SharingStarted.WhileSubscribed(5),
+            initialValue = initialSeed
+        )
 
     private val _isPlayingHolder = MutableStateFlow<Boolean>(false)
     val isPlayingHolder :StateFlow<Boolean>
@@ -34,6 +61,23 @@ abstract class CustomPlayerNew(val dataStoreRepository:DataStoreRepository) {
 
     abstract fun shuffleAction()
 
+    fun setMusic(music: Music,context: Context){
+        val isMusicValid = music.fileDir != "empty"
+        if (isMusicValid) {
+            mediaPlayer.apply {
+                reset()
+                setDataSource(context, music.getUri())
+                setOnCompletionListener {
+                    if (!isLooping) {
+                        context.sendBroadcast(nextIntent)
+                    }
+                }
+                prepare()
+            }
+        }
+    }
+
+
     suspend fun isShuffled_Setter(isShuffled:Boolean){
         dataStoreRepository.saveIsShuffled(isShuffled)
     }
@@ -42,10 +86,11 @@ abstract class CustomPlayerNew(val dataStoreRepository:DataStoreRepository) {
         dataStoreRepository.saveIsLooping(isLooping)
     }
 
+    suspend fun seed_Setter(seed:Double){
+        dataStoreRepository.saveRandomSeed(seed)
+    }
+
     fun isPlaying_Setter(isPlaying:Boolean){
         _isPlayingHolder.value = isPlaying
     }
-
-
-
 }
--- app/src/main/java/com/todokanai/musicplayer/player/CustomPlayer_R.kt
+++ app/src/main/java/com/todokanai/musicplayer/player/CustomPlayer_R.kt
@@ -3,32 +3,11 @@ package com.todokanai.musicplayer.player
 import com.todokanai.musicplayer.data.datastore.DataStoreRepository
 import com.todokanai.musicplayer.data.room.Music
 import com.todokanai.musicplayer.repository.MusicRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 
 class CustomPlayer_R(
     val musicRepo:MusicRepository,
     val dsRepo:DataStoreRepository,
-    val scope:CoroutineScope
 ):CustomPlayerNew(dsRepo) {
-
-    private val initialLoop = false
-    private val initialShuffle = false
-
-    override val isShuffledHolder: StateFlow<Boolean>
-        get() = dataStoreRepository.isShuffled.stateIn(
-            scope = scope,
-            started = SharingStarted.WhileSubscribed(5),
-            initialValue = initialShuffle
-        )
-    override val isLoopingHolder: StateFlow<Boolean>
-        get() = dataStoreRepository.isShuffled.stateIn(
-            scope = scope,
-            started = SharingStarted.WhileSubscribed(5),
-            initialValue = initialLoop
-        )
 
     override fun setMusic(music: Music) {
         TODO("Not yet implemented")
