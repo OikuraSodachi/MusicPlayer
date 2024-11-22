801a5ebefeeb6585961423b50b39a1c88ef44607
Todokanai <bh951201@gmail.com>
2024-11-22 14:13:20 +0900

    주석 추가

--- app/src/main/java/com/todokanai/musicplayer/tools/independent/Etc.kt
+++ app/src/main/java/com/todokanai/musicplayer/tools/independent/Etc.kt
@@ -1,5 +1,10 @@
 package com.todokanai.musicplayer.tools.independent
 
+/**
+ * @param [list] list of items
+ * @param [current] current item
+ * @return next element of the [list], or the first element(if [current] is the last element). -1 if [list] does not contain [current]
+ * **/
 fun <Type:Any> getCircularNext_td(list:List<Type>, current:Type):Type{
     val currentIndex = list.indexOf(current)
     return if(currentIndex == list.size-1){
@@ -9,6 +14,11 @@ fun <Type:Any> getCircularNext_td(list:List<Type>, current:Type):Type{
     }
 }
 
+/**
+ * @param [list] list of items
+ * @param [current] current item
+ * @return previous element of the [list], or the last element( if [current] is the first element )
+ * **/
 fun <Type:Any> getCircularPrev_td(list:List<Type>,current:Type):Type{
     val currentIndex = list.indexOf(current)
     return if(currentIndex == 0){
