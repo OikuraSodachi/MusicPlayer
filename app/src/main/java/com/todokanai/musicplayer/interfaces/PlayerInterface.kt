cca30fc85cdebc1f9459cc5adce8eec7fbdbd639
Todokanai <bh951201@gmail.com>
2025-05-23 10:36:07 +0900

    temp
    일단 CustomPlayer 제거함.

--- app/src/main/java/com/todokanai/musicplayer/interfaces/PlayerInterface.kt
+++ app/src/main/java/com/todokanai/musicplayer/interfaces/PlayerInterface.kt
@@ -1,30 +1,23 @@
 package com.todokanai.musicplayer.interfaces
 
 import android.content.Context
-import android.support.v4.media.session.MediaSessionCompat
 import com.todokanai.musicplayer.data.room.Music
 
 interface PlayerInterface {
 
   //  fun initAttributes(context: Context)
 
-    /** returns previous item of playList **/
-    fun getPrevMusic(): Music
-
-    /** returns next item of playList **/
-    fun getNextMusic(): Music
+//    /** returns previous item of playList **/
+//    fun getPrevMusic(): Music
+//
+//    /** returns next item of playList **/
+//    fun getNextMusic(): Music
 
     fun launchMusic(context: Context, music: Music)
 
-    fun applyShuffle()
+  //  fun applyShuffle()
 
     fun onMusicListScan(context: Context)
-
-    fun requestUpdateNoti(mediaSession: MediaSessionCompat, startForegroundService:()->Unit)
-
-    abstract fun notification(context: Context)
-
-    fun startForeground(context: Context)
 
     fun repeatAction(context: Context)
 
