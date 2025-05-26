d7620863ee4a75827921f8eeb905b471aa114c5d
Todokanai <bh951201@gmail.com>
2025-05-26 17:10:08 +0900

    SavableStateFlow refactor.
    Safe

--- app/src/main/java/com/todokanai/musicplayer/tools/independent/SavableStateFlow.kt
+++ app/src/main/java/com/todokanai/musicplayer/tools/independent/SavableStateFlow.kt
@@ -2,18 +2,19 @@ package com.todokanai.musicplayer.tools.independent
 
 import kotlinx.coroutines.flow.MutableStateFlow
 
-/** MutableStateFlow designed for saving last known value
- * @author Oikura Sodachi
- * @param originalStateFlow original MutableStateFlow
- * @param saveValue save value to database **/
-data class SavableStateFlow<Type:Any>(
-    private val originalStateFlow:MutableStateFlow<Type>,
-    private val saveValue:(value:Type)->Unit
+/** MutableStateFlow for saving last known value
+ * @param initialValue the initial value
+ * @param saveValue callback to save value to database **/
+class SavableStateFlow<Type:Any>(
+    initialValue:Type,
+    private val saveValue:(value:Type)->Unit,
+    private val originalStateFlow : MutableStateFlow<Type> = MutableStateFlow(initialValue)
 ): MutableStateFlow<Type> by originalStateFlow {
+
     override var value: Type
         get() = originalStateFlow.value
-        set(newValue) {
-            originalStateFlow.value = newValue
-            saveValue
+        set(value) {
+            originalStateFlow.value = value
+            saveValue(value)
         }
 }
