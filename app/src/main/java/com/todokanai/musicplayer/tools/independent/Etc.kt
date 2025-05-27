203f99c04d851b7f726ad32263e4c7b75ccf1b21
Todokanai <bh951201@gmail.com>
2025-05-28 06:40:54 +0900

    Todo: PlayerStateRepository 에서 NewPlayer 제거 할 것. ( data 흐름 역방향임 )
    getCircularNext_td, getCircularPrev_td refactor.

--- app/src/main/java/com/todokanai/musicplayer/tools/independent/Etc.kt
+++ app/src/main/java/com/todokanai/musicplayer/tools/independent/Etc.kt
@@ -1,29 +1,25 @@
 package com.todokanai.musicplayer.tools.independent
 
-/**
- * @param [list] list of items
- * @param [current] current item
- * @return next element of the [list], or the first element(if [current] is the last element). -1 if [list] does not contain [current]
- * **/
-fun <Type:Any> getCircularNext_td(list:List<Type>, current:Type):Type{
-    val currentIndex = list.indexOf(current)
-    return if(currentIndex == list.size-1){
-        list.first()
-    } else{
-        list[currentIndex+1]
+/** @param [list] list of items
+ * @param [currentItem] current item
+ * @return next element of the [list], or the first element(if [currentItem] is the last element)
+ * @throws Exception if [currentItem] is not found in [list] **/
+fun <Type:Any> getCircularNext_td(list:List<Type>, currentItem:Type):Type{
+    return when (list.indexOf(currentItem)){
+        -1 -> throw Exception("item not found in list")
+        0 -> list.last()
+        else -> list[list.indexOf(currentItem)-1]
     }
 }
 
-/**
- * @param [list] list of items
- * @param [current] current item
- * @return previous element of the [list], or the last element( if [current] is the first element )
- * **/
-fun <Type:Any> getCircularPrev_td(list:List<Type>,current:Type):Type{
-    val currentIndex = list.indexOf(current)
-    return if(currentIndex == 0){
-        list.last()
-    } else{
-        list[currentIndex-1]
+/** @param [list] list of items
+ * @param [currentItem] current item
+ * @return previous element of the [list], or the last element(if [currentItem] is the first element)
+ * @throws Exception if [currentItem] is not found in [list] **/
+fun <Type:Any> getCircularPrev_td(list:List<Type>, currentItem:Type):Type{
+    return when (list.indexOf(currentItem)){
+        -1 -> throw Exception("item not found in list")
+        0 -> list.last()
+        else -> list[list.indexOf(currentItem)-1]
     }
 }
