c4e2cc57113f9a5ab4fd5364f2dd2a55a4f4af5e
Todokanai <bh951201@gmail.com>
2025-06-02 09:05:23 +0900

    NoisyReceiver 에서 NewPlayer reference 제거.
    Safe

--- app/src/main/java/com/todokanai/musicplayer/components/receiver/NoisyReceiver.kt
+++ app/src/main/java/com/todokanai/musicplayer/components/receiver/NoisyReceiver.kt
@@ -4,21 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
-import com.todokanai.musicplayer.player.NewPlayer
+import android.media.MediaPlayer
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class NoisyReceiver : BroadcastReceiver() {
-  //  @Inject
-    //lateinit var player:CustomPlayer
 
     @Inject
-    lateinit var player:NewPlayer
-    //private val player by lazy{getPlayer}
+    lateinit var player: MediaPlayer
+
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == AudioManager.ACTION_AUDIO_BECOMING_NOISY) {
-            if(player.isPlaying()) {
+            if(player.isPlaying) {
                 player.pause()
             }
         }
