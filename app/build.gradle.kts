4df24b002210b999d5c102219ff38bc689fc7c53
Todokanai <bh951201@gmail.com>
2025-07-30 08:57:39 +0900

    PlayStore 제출 버전
    compose preview 제거
    
    Safe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.todokanai.musicplayer"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
