7489eff96c1285bf0ae550d6d27a230386cd0beb
Todokanai <bh951201@gmail.com>
2025-07-31 07:13:56 +0900

    PlayStore 제출 버전 target Api 35 ( AndroidManifest )
    Safe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.todokanai.musicplayer"
         minSdk = 30
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
